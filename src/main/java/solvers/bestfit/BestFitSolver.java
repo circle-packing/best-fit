c6f947b52e9e78207dd2aeeb208a2e2b1a4c7aea
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-11-29 15:02:42 +0100

    Broke algorithm into smaller parts.

--- src/main/java/solvers/bestfit/BestFitSolver.java
+++ src/main/java/solvers/bestfit/BestFitSolver.java
@@ -14,69 +14,88 @@ import java.util.*;
  */
 public class BestFitSolver extends Solver {
 
+	private List<Circle> circlesToPack; //The remaining circles to pack, for all see the Problem
+
+	static private Comparator<Hole> holeComparer = new Comparator<Hole>() {
+		@Override
+		public int compare(Hole o1, Hole o2) {
+			return Double.compare(o2.getSize(), o1.getSize());
+		}
+	};
+	private PriorityQueue<Hole> holes;
+
+	static private Comparator<SideMount> mountComparer = new Comparator<SideMount>() {
+		@Override
+		public int compare(SideMount o1, SideMount o2) {
+			return Double.compare(o2.getSize(), o1.getSize());
+		}
+	};
+	private PriorityQueue<SideMount> mounts;
+
+
 	public BestFitSolver(@NotNull Problem problem) {
 		super(problem);
 	}
 
-	@Override
-	public void solve() {
+	private void init() {
+		// Problem
 		Problem problem = getProblem();
-		List<Circle> circles = problem.getCircles();
-		Solution solution = new Solution(circles.size());
+		circlesToPack = new ArrayList<>(problem.getCircles());
 
-		if (problem.getCircles().size() < 3) {
+		if (circlesToPack.size() < 3) {
 			throw new IllegalStateException("Too little circles in this problem, need at least 3.");
 		}
 
-		Collections.sort(circles, new Comparator<Circle>() {
+		Collections.sort(circlesToPack, new Comparator<Circle>() {
 			@Override
 			public int compare(Circle c1, Circle c2) {
 				return Double.compare(c2.getRadius(), c1.getRadius());
 			}
 		});
 
+		// Solution
+		setSolution(new Solution(circlesToPack.size()));
+
+		// Collections
+		holes = new PriorityQueue<>(circlesToPack.size(), holeComparer);
+		mounts = new PriorityQueue<>(circlesToPack.size(), mountComparer);
+	}
+
+	private void packFirstThree() {
 		//Initially place the two biggest circles next to eachother
-		Circle first = circles.get(0);
-		Circle second = circles.get(1);
+		Circle first = circlesToPack.get(0);
+		Circle second = circlesToPack.get(1);
 
 		Vector2 firstPos = new Vector2(0, 0);
 		Vector2 secondPos = new Vector2(first.getRadius() + second.getRadius(), 0);
-		
-		solution.add(first, firstPos);
-		solution.add(second, secondPos);
+
+		getSolution().add(first, firstPos);
+		getSolution().add(second, secondPos);
 
 		// Place the third biggest circle on top of the first two (assuming they are positioned clockwise)
 		SideMount mountThird = new SideMount(firstPos, secondPos, first, second);
-		Circle third = circles.get(2);
+		Circle third = circlesToPack.get(2);
 		Vector2 thirdPos = mountThird.getMountPositionFor(third);
-		solution.add(third, thirdPos);
+		getSolution().add(third, thirdPos);
 
-		// TODO
-		// Create initial holes, fill biggest holes first
-		Comparator<Hole> holeComparer = new Comparator<Hole>() {
-			@Override
-			public int compare(Hole o1, Hole o2) {
-				return Double.compare(o2.getSize(), o1.getSize());
-			}
-		};
-		PriorityQueue<Hole> holes = new PriorityQueue<>(circles.size(), holeComparer);
+		circlesToPack.remove(first);
+		circlesToPack.remove(second);
+		circlesToPack.remove(third);
+
+		// Create first hole
 		holes.add(new Hole(firstPos, secondPos, thirdPos, first, second, third));
-		// Create initial mountpoints, place in biggest mountpoints first
-		Comparator<SideMount> mountComparer = new Comparator<SideMount>() {
-			@Override
-			public int compare(SideMount o1, SideMount o2) {
-				return Double.compare(o2.getSize(), o1.getSize());
-			}
-		};
-		PriorityQueue<SideMount> mounts = new PriorityQueue<>(circles.size(), mountComparer);
+		// Create first mount points
+		// TODO
+	}
+
+	private void doBestFit() {
 		// Do best-fit
-		List<Circle> circlesToPlace = new ArrayList<>(circles);
-		while(!circlesToPlace.isEmpty()) {
+		while(!circlesToPack.isEmpty()) {
 			if(!holes.isEmpty()) {
 				Hole toFill = holes.remove(); //removes the hole, so remembers we already handled it
 				double size = toFill.getSize();
 				// IMPORTANT: circlesToPlace must be ordered big to small!
-				Circle bestFit = biggestNotLargerThan(size, circlesToPlace);
+				Circle bestFit = biggestNotLargerThan(size, circlesToPack);
 
 				if (bestFit == null) { //none fit
 					continue; //just go on with the algo, this hole can't be filled
@@ -84,9 +103,9 @@ public class BestFitSolver extends Solver {
 
 				// place the best fit
 				Vector2 center = toFill.getCenter();
-				solution.add(bestFit, center);
+				getSolution().add(bestFit, center);
 				//remember as already been placed
-				circlesToPlace.remove(bestFit);
+				circlesToPack.remove(bestFit);
 
 				//create new holes
 				holes.addAll(toFill.getHolesWhenFilledWith(bestFit, center, size));
@@ -99,13 +118,20 @@ public class BestFitSolver extends Solver {
 				break;
 			}
 		}
+	}
 
+	@Override
+	public void solve() {
+		init();
 
-		// Set solution
-		setSolution(solution);
-		// Check if solution is valid
-		System.out.println("Error/Total overlap: " + solution.calculateError());
-		System.out.println("Packed " + solution.getLocations().size() + " of " + problem.getCircles().size() + " circles.");
+		packFirstThree();
+
+		doBestFit();
+
+		// Print some information
+		System.out.println("Error/Total overlap: " + getSolution().calculateError());
+		System.out.println("Packed " + getSolution().getLocations().size() + " of " + getProblem().getCircles().size() + " circles.");
+		System.out.println("" + circlesToPack.size() + " still need to be packed.");
 	}
 
 	private Circle biggestNotLargerThan(double size, List<Circle> sortedBigToSmall) {
